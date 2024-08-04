7f6b4ca4190dd441a4c6f336fa269a13ce8994f9
Sujal <sujalkumar7209@gmail.com>
2024-08-04 19:55:09 +0530

    modified:   package-lock.json 	modified:   package.json 	modified:   src/config.js 	modified:   src/index.js 	modified:   views/login.ejs 	modified:   views/signup.ejs

--- src/index.js
+++ src/index.js
@@ -4,13 +4,11 @@ const collection = require("./config");
 const bcrypt = require('bcrypt');
 
 const app = express();
-// convert data into json format
 app.use(express.json());
-// Static file
 app.use(express.static("public"));
 
 app.use(express.urlencoded({ extended: false }));
-//use EJS as the view engine
+
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
@@ -21,7 +19,7 @@ app.get("/signup", (req, res) => {
     res.render("signup");
 });
 
-// Register User
+
 app.post("/signup", async (req, res) => {
 
     const data = {
@@ -29,17 +27,15 @@ app.post("/signup", async (req, res) => {
         password: req.body.password
     }
 
-    // Check if the username already exists in the database
     const existingUser = await collection.findOne({ name: data.name });
 
     if (existingUser) {
         res.send('User already exists. Please choose a different username.');
     } else {
-        // Hash the password using bcrypt
-        const saltRounds = 10; // Number of salt rounds for bcrypt
+        const saltRounds = 10; 
         const hashedPassword = await bcrypt.hash(data.password, saltRounds);
 
-        data.password = hashedPassword; // Replace the original password with the hashed one
+        data.password = hashedPassword; 
 
         const userdata = await collection.insertMany(data);
         console.log(userdata);
@@ -47,14 +43,12 @@ app.post("/signup", async (req, res) => {
 
 });
 
-// Login user 
 app.post("/login", async (req, res) => {
     try {
         const check = await collection.findOne({ name: req.body.username });
         if (!check) {
             res.send("User name cannot found")
         }
-        // Compare the hashed password from the database with the plaintext password
         const isPasswordMatch = await bcrypt.compare(req.body.password, check.password);
         if (!isPasswordMatch) {
             res.send("wrong Password");
@@ -69,7 +63,6 @@ app.post("/login", async (req, res) => {
 });
 
 
-// Define Port for Application
 const port = 5000;
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`)
